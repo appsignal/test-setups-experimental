41158fdd7b211b8023870d0ba3dcd96cf0e746ec
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-12-03 10:26:05 +0100

    Add :appsignal_plug to deps

--- elixir/plug/app/mix.exs
+++ elixir/plug/app/mix.exs
@@ -22,7 +22,8 @@ defmodule App.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:appsignal_plug, "~> 2.0"}
     ]
   end
 end
